842299fdcfc78a9233c798853b727ef8c879d792
Clément DOUIN <soywod@users.noreply.github.com>
2019-12-04 14:37:59 +0100

    add sign in providers (google and facebook)

--- src/auth/login.tsx
+++ src/auth/login.tsx
@@ -1,26 +1,23 @@
 import React, {FC, useEffect, useState} from "react"
 import {RouteComponentProps} from "react-router-dom"
-import {Form, Field} from "react-final-form"
 
 import Link from "../shared/link"
-import useAuth, {AuthCredentials} from "./context"
+import useAuth from "./context"
 import $auth from "./service"
 
 import classes from "./login.module.scss"
 
-type Step = "email" | "password"
+type Step = "email" | "password" | "connect"
+
+type LoginFormProps = {
+  nextStep: (field: string) => void
+}
 
 const Login: FC<RouteComponentProps> = props => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
   const [step, setStep] = useState<Step>("email")
   const auth = useAuth()
-
-  async function nextStep(credentials: AuthCredentials) {
-    try {
-      await $auth.login(credentials)
-    } catch (err) {
-      console.log(err)
-    }
-  }
 
   useEffect(() => {
     if (auth.state.authenticated) {
@@ -32,77 +29,26 @@ const Login: FC<RouteComponentProps> = props => {
     return null
   }
 
-  const renderFields = () => {
+  const renderForm = () => {
     switch (step) {
       case "email":
         return (
-          <>
-            <div className={classes.login}>
-              <label className={classes.label}>Adresse email</label>
-              <Field
-                autoFocus
-                className={classes.input}
-                name="email"
-                component="input"
-                type="email"
-              />
-            </div>
-
-            <button className={classes.continue} type="submit">
-              Continuer
-            </button>
-
-            <div className={classes.separator}>
-              <span>ou</span>
-            </div>
-
-            <div className={classes.otherContinues}>
-              <button className={classes.continueWithGoogle} type="button">
-                Continuer avec Google
-              </button>
-              <button className={classes.continueWithFacebook} type="button">
-                Continuer avec Facebook
-              </button>
-            </div>
-
-            <div>
-              <Link className={classes.link} to="/forgotten-password">
-                Créer un compte
-              </Link>
-            </div>
-          </>
+          <EmailStep
+            nextStep={nextEmail => {
+              setEmail(nextEmail)
+              setStep("password")
+            }}
+          />
         )
 
       case "password":
         return (
-          <>
-            <div className={classes.formItem}>
-              <label className={classes.label}>Login</label>
-              <Field
-                autoFocus
-                className={classes.input}
-                name="email"
-                component="input"
-                type="email"
-              />
-            </div>
-
-            <div className={classes.formItem}>
-              <label className={classes.label}>Password</label>
-              <Field className={classes.input} name="password" component="input" type="password" />
-              <div>
-                <Link className={classes.link} to="/forgotten-password">
-                  Mot de passe oublié
-                </Link>
-              </div>
-            </div>
-
-            <div>
-              <button className={classes.submit} type="submit">
-                Connecter
-              </button>
-            </div>
-          </>
+          <PasswordStep
+            nextStep={nextPassword => {
+              setPassword(nextPassword)
+              setStep("connect")
+            }}
+          />
         )
 
       default:
@@ -113,15 +59,113 @@ const Login: FC<RouteComponentProps> = props => {
   return (
     <div className={classes.container}>
       <img className={classes.logo} src="/images/logo.svg" alt="" />
-      <Form
-        onSubmit={nextStep}
-        render={({handleSubmit}) => (
-          <form className={classes.form} onSubmit={handleSubmit}>
-            {renderFields()}
-          </form>
-        )}
-      />
+      {renderForm()}
     </div>
+  )
+}
+
+const EmailStep: FC<LoginFormProps> = ({nextStep}) => {
+  const [email, setEmail] = useState("")
+
+  function handleChange(evt: React.ChangeEvent<HTMLInputElement>) {
+    setEmail(evt.target.value)
+  }
+
+  function handleSubmit(evt: React.FormEvent<HTMLFormElement>) {
+    evt.preventDefault()
+    nextStep(email)
+  }
+
+  async function loginWithGoogle() {
+    try {
+      await $auth.loginWithGoogle()
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  async function loginWithFacebook() {
+    try {
+      await $auth.loginWithFacebook()
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  return (
+    <form className={classes.form} onSubmit={handleSubmit}>
+      <div className={classes.login}>
+        <label className={classes.label}>Adresse email</label>
+        <input
+          className={classes.input}
+          type="email"
+          name="email"
+          autoFocus
+          onChange={handleChange}
+        />
+      </div>
+
+      <button className={classes.continue} type="submit">
+        Continuer
+      </button>
+
+      <div>
+        <Link className={classes.link} to="/register">
+          Créer un compte
+        </Link>
+      </div>
+
+      <div className={classes.separator}>
+        <span>ou</span>
+      </div>
+
+      <div className={classes.otherContinues}>
+        <button className={classes.continueWithGoogle} type="button" onClick={loginWithGoogle}>
+          Continuer avec Google
+        </button>
+        <button className={classes.continueWithFacebook} type="button" onClick={loginWithFacebook}>
+          Continuer avec Facebook
+        </button>
+      </div>
+    </form>
+  )
+}
+
+const PasswordStep: FC<LoginFormProps> = ({nextStep}) => {
+  const [password, setPassword] = useState("")
+
+  function handleChange(evt: React.ChangeEvent<HTMLInputElement>) {
+    setPassword(evt.target.value)
+  }
+
+  function handleSubmit(evt: React.FormEvent<HTMLFormElement>) {
+    evt.preventDefault()
+    nextStep(password)
+  }
+
+  return (
+    <form className={classes.form} onSubmit={handleSubmit}>
+      <div className={classes.login}>
+        <label className={classes.label}>Mot de passe</label>
+        <input
+          className={classes.input}
+          type="password"
+          name="password"
+          autoFocus
+          onChange={handleChange}
+        />
+      </div>
+
+      <button className={classes.continue} type="submit">
+        Continuer
+      </button>
+
+      <div>
+        <Link className={classes.link} to="/forgotten-password">
+          Mot de passe oublié
+        </Link>
+      </div>
+    </form>
   )
 }
 
--- src/auth/service.ts
+++ src/auth/service.ts
@@ -1,12 +1,22 @@
-import {auth} from "../shared/firebase"
+import firebase, {auth} from "../shared/firebase"
 import {AuthCredentials} from "./context"
 
-export async function login({email, password}: AuthCredentials) {
+export async function loginWithCredentials({email, password}: AuthCredentials) {
   return auth.signInWithEmailAndPassword(email, password).then(res => res.user)
 }
 
-export async function logout() {
-  await auth.signOut()
+export function loginWithGoogle() {
+  const provider = new firebase.auth.GoogleAuthProvider()
+  return auth.signInWithPopup(provider)
 }
 
-export default {login, logout}
+export function loginWithFacebook() {
+  const provider = new firebase.auth.FacebookAuthProvider()
+  return auth.signInWithPopup(provider)
+}
+
+export function logout() {
+  return auth.signOut()
+}
+
+export default {loginWithCredentials, loginWithGoogle, loginWithFacebook, logout}
