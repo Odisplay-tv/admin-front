5d3c7ef1bd63c908ca65d38e63b6e9d3066c1788
Clément DOUIN <soywod@users.noreply.github.com>
2019-12-18 17:26:10 +0100

    integrate screen list

--- src/screen/list.tsx
+++ src/screen/list.tsx
@@ -1,6 +1,7 @@
 import React, {FC} from "react"
 import {useTranslation} from "react-i18next"
 
+import Link from "../app/link"
 import useScreens from "./context"
 
 import classes from "./list.module.scss"
@@ -31,12 +32,19 @@ const ScreenList: FC = () => {
             </tr>
           </thead>
           <tbody>
+            {screens.length === 0 && (
+              <tr className={classes.emptyRow}>
+                <td className={classes.emptyCol} colSpan={6}>
+                  <div>{t("screen:empty-list")}</div>
+                </td>
+              </tr>
+            )}
             {screens.map(s => (
               <tr key={s.id} className={classes.row}>
-                <td className={classes.selectRow}>
+                <td className={classes.selectCol}>
                   <input type="checkbox" />
                 </td>
-                <td className={classes.statusRow}>
+                <td className={classes.statusCol}>
                   <svg
                     className={classes.connected}
                     viewBox="0 0 2 2"
@@ -45,12 +53,12 @@ const ScreenList: FC = () => {
                     <circle cx="1" cy="1" r="1" />
                   </svg>
                 </td>
-                <td className={classes.nameRow}>{s.name}</td>
-                <td className={classes.layoutRow}>{null}</td>
-                <td className={classes.settingsRow}>
+                <td className={classes.nameCol}>{s.name}</td>
+                <td className={classes.layoutCol}>{null}</td>
+                <td className={classes.settingsCol}>
                   <button type="button">settings</button>
                 </td>
-                <td className={classes.deleteRow}>
+                <td className={classes.deleteCol}>
                   <button type="button" onClick={handleDelete(s.id)}>
                     delete
                   </button>
@@ -59,6 +67,16 @@ const ScreenList: FC = () => {
             ))}
           </tbody>
         </table>
+        <aside className={classes.aside}>
+          <Link className={classes.connectScreen} to="/screens/connect">
+            <img src="/images/screen.svg" alt="" />
+            <span dangerouslySetInnerHTML={{__html: t("screen:connect-title")}} />
+          </Link>
+          <button className={classes.createGroup} type="button">
+            <img src="" alt="" />
+            <span>{t("screen:create-group")}</span>
+          </button>
+        </aside>
       </div>
     </div>
   )
